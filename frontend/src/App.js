b2d202e89302ea159800cdcb7e11927622864a63
DONRAJI <likesea554@naver.com>
2025-11-17 15:41:33 +0900

    2021dssㅊㅊ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import { BrowserRouter, Routes, Route, Navigate, useNavigate, useParams } from 'react-router-dom';
 import SplashScreen from './components/SplashScreen/SplashScreen';
